61cb990ed17f636d4bc944d6447f2c42929ed82b
mickdeijs <341385@student.mboutrecht.nl>
2025-03-19 14:41:51 +0100

    Eerste commit

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * De database verbindingsgegevens
+ */
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'BE-proeftoets-periode-3');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+
+/**
+ * De naam van de virtualhost
+ */
+define('URLROOT', 'http://be-proeftoets-periode-3');
+
+/**
+ * Het pad naar de folder app
+ */
+define('APPROOT', dirname(dirname(__FILE__)));
